4f73b24ffb8d0e1db671a8b4813167782bf86680
OmegaM <omegamiao@gmail.com>
2016-08-12 22:23:45 +0800

    init project

---
+++ src/main/java/com/sigma/dp/InitMain.java
@@ -0,0 +1,14 @@
+package com.sigma.dp;
+
+/**
+ * Created with IDEA
+ * User: Omega
+ * Date: 16/8/12
+ * Time: 22:09
+ */
+public class InitMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
