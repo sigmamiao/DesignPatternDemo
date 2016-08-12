1342ba44c6029c3ac6edfc0ea8c2915cf4b03794
OmegaM <omegamiao@gmail.com>
2016-08-12 23:00:50 +0800

    add println

--- src/main/java/com/sigma/dp/InitMain.java
+++ src/main/java/com/sigma/dp/InitMain.java
@@ -10,5 +10,7 @@ public class InitMain {
 
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        //TODO
     }
 }
